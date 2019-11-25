9e73fd6fe8ec71b5542f5a4045c3c7abd76c4c6b
Donna Hedley <donna.hedley@shopify.com>
2019-11-25 13:28:58 -0500

    updated most rubocop fixes

--- address_book.rb
+++ address_book.rb
@@ -1,4 +1,4 @@
-
+# frozen_string_literal: true
 
 ## To do
 #  sort
@@ -9,140 +9,127 @@ require_relative 'classes.rb'
 require_relative 'methods.rb'
 
 ## initialize varilables
-fname = ''
-lname = ''
-phone = ''
 file_name = 'people_data.yml'
 all_people = read_records(file_name)
-
-
 
 # display menu items until user exits
 picker = true
 while picker do
-    display_main_menu
+  display_main_menu
+  choice = gets.chomp.to_i
+  case choice
+  when 1 # Adding new record
+    # get values from user
+    person = Person.new(0, 0, 0, 0, 0)
+    person.id = get_next_id(all_people)
+
+    puts '=========== New Record ==================\n'
+
+    set_first_name(person)
+    set_last_name(person)
+    set_phone_number(person)
+    set_new_address(person)
+
+    select = 'y'
+    while select.downcase == 'y'
+      puts "\n Do you want to enter another address? (Y/N)\n"
+      select = gets.chomp
+      set_new_address(person) if select.downcase == 'y'
+    end
+    all_people.push(person)
+    puts "\n*********** Record Added ***************"
+  when 2 # Updating a record
+    puts "\n*********** Update Record ***************"
+
+    # find Record to delete
+    display_record_names(all_people)
+
+    puts "\n----------------\n"
+    print "\nSelect ID of record to update: "
     choice = gets.chomp.to_i
-    case choice
-    when 1 # Adding new record
-        #get values from user
+    puts "\n----------------\n"
 
-        person = Person.new(0,0,0,0,0)
+    temp = all_people.find { |l| l.id == choice }
 
-        person.id = get_next_id(all_people)
+    index = all_people.index { |x| x.id == temp.id } #=> 1
 
-        puts "=========== New Record ==================\n"
+    # confirm that they really want to delete this record
+    print "You want to update record #{temp.id}: for #{temp.full_name}? (Y/N)"
 
-        set_first_name(person)
-        set_last_name(person)
-        set_phone_number(person)
-        set_new_address(person)
-
-        select = 'y'
-        while select.downcase == 'y'
-            puts "\n Do you want to enter another address? (Y/N)\n"
-            select = gets.chomp
-            if select.downcase == 'y'
-                set_new_address(person)
-            end
+    select = gets.chomp
+    if select.downcase == 'y'
+      # update
+      temp_arr = []
+      temp_arr.push(temp)
+      display_records(temp_arr)
+      edit_var = true
+      while edit_var == true
+        ## ask what part they want to Update
+        display_update_menu
+        edit = gets.chomp.to_i
+        case edit
+        when 1
+          set_first_name(temp)
+        when 2
+          set_last_name(temp)
+        when 3
+          set_phone_number(temp)
+        when 4
+          set_first_name(temp)
+          set_last_name(temp)
+          set_phone_number(temp)
+        when 5
+          edit_var = false
+        else
+          puts 'Enter a value between 1 and 5'
         end
-
-
-
-        all_people.push(person)
-        puts "\n*********** Record Added ***************"
-      when 2 # Updating a record
-        puts "\n*********** Update Record ***************"
-
-        # find Record to delete
-        display_record_names(all_people)
-
-        puts = "\n----------------\n"
-        print "\nSelect ID of record to update: "
-        choice = gets.chomp.to_i
-        puts = "\n----------------\n"
-
-        temp = all_people.find { |l| l.id == choice }
-
-        index = all_people.index { |x| x.id == temp.id }  #=> 1
-
-        # confirm that they really want to delete this record
-        print "You want to update record #{temp.id}: for #{temp.full_name}? (Y/N)"
-
-        select = gets.chomp
-        if select.downcase == 'y'
-          # update
-          temp_arr = []
-          temp_arr.push(temp)
-          display_records(temp_arr)
-          edit_var = true
-          while edit_var == true
-              ## ask what part they want to Update
-              display_update_menu
-              edit = gets.chomp.to_i
-              case edit
-                  when 1
-                    set_first_name(temp)
-                  when 2
-                  set_last_name(temp)
-                  when 3
-                    set_phone_number(temp)
-                  when 4
-
-                    set_first_name(temp)
-                    set_last_name(temp)
-                    set_phone_number(temp)
-
-                  when 5
-                    edit_var = false
-                  else
-                    puts "Enter a value between 1 and 5"
-              end
-              all_people[index] = temp
-          end
-          # give message that record has been deleted
-          puts "\n*********** Record Updated ***************"
-        end
-
-      when 3 # Deleteing a record
-        puts "\n*********** Delete Record ***************"
-        # find Record to delete
-        display_record_names(all_people)
-
-        puts = "\n----------------\n"
-
-        print "\nSelect ID of record to delete: "
-        choice = gets.chomp.to_i
-
-        puts = "\n----------------\n"
-        # to do: get ID -- have to come up with better way to retrieve ID because they might be deleted
-        temp = all_people.find { |l| l.id == choice }
-        # confirm that they really want to delete this record
-        print "Are you sure you want to delete record #{temp.id}: for #{temp.full_name} (Y/N)"
-        select = gets.chomp
-
-        if select.downcase == 'y'
-          # Delete
-          all_people.delete_if  {|x| x.id == temp.id }
-
-          # give message that record has been deleted
-          puts "\n*********** Record Deleted ***************"
-        end
-
-      when 4 #Displaying a record
-        # add a filter and find feature
-          puts "\n*********** All Records ********************"
-          display_records(all_people)
-          puts "\n****** Total Records: #{all_people.length} ******\n\n\n"
-          print "Return"
-          gets.chomp
-      when 5 # Updating data and exiting program
-          puts "Bye, Bye!"
-        #  display_records(all_people)
-          save_records(all_people, file_name)
-          picker = false
-    #  when 9 #Testing
-    #    puts "Next ID is : #{get_next_id(all_people)}"
-      else
-        puts "Please use a value between 1 and 6"
+        all_people[index] = temp
       end
+      # give message that record has been deleted
+      puts "\n*********** Record Updated ***************"
+    end
+
+  when 3 # Deleteing a record
+    puts "\n*********** Delete Record ***************"
+    # find Record to delete
+    display_record_names(all_people)
+
+    puts "\n----------------\n"
+
+    print "\nSelect ID of record to delete: "
+    choice = gets.chomp.to_i
+
+    puts "\n----------------\n"
+    # to do: get ID -- have to come up with better way to retrieve ID because
+    # they might be deleted
+    temp = all_people.find { |l| l.id == choice }
+    # confirm that they really want to delete this record
+    print "Are you sure you want to delete record #{temp.id}: for #{temp.full_name} (Y/N)"
+    select = gets.chomp
+
+    if select.downcase == 'y'
+      # Delete
+      all_people.delete_if { |x| x.id == temp.id }
+
+      # give message that record has been deleted
+      puts "\n*********** Record Deleted ***************"
+    end
+
+  when 4 # Displaying a record
+    # add a filter and find feature
+    puts "\n*********** All Records ********************"
+    display_records(all_people)
+    puts "\n****** Total Records: #{all_people.length} ******\n\n\n"
+    print 'Return'
+    gets.chomp
+  when 5 # Updating data and exiting program
+    puts 'Bye, Bye!'
+    # display_records(all_people)
+    save_records(all_people, file_name)
+    picker = false
+    # when 9 #Testing
+    # puts "Next ID is : #{get_next_id(all_people)}"
+  else
+    puts 'Please use a value between 1 and 6'
+  end
 end
--- methods.rb
+++ methods.rb
@@ -1,55 +1,48 @@
 def read_records(file_name)
   arr = []
-    stuff = YAML.load(File.read(file_name))
-    stuff.each do |r|
-      stuff2 = r.addressess
-      stuff2.each do |a|
-        address = Address.new(a.kind, a.street, a.city, a.province, a.postal_code)
-        person = Person.new(r.first_name, r.last_name, r.phone, r.id, 0)
-        person.addressess.push(address)
-        arr.push(person)
-      end
+  stuff = YAML.load(File.read(file_name))
+  stuff.each do |r|
+    stuff2 = r.addressess
+    stuff2.each do |a|
+      address = Address.new(a.kind, a.street, a.city, a.province, a.postal_code)
+      person = Person.new(r.first_name, r.last_name, r.phone, r.id, 0)
+      person.addressess.push(address)
+      arr.push(person)
     end
-    return arr
+  end
+  arr
 end
 
 def save_records(records, file_name)
-
-
-    if File.file?(file_name)==true
-      # "FILE EXISTS"
-        output = File.new(file_name, 'w')
-    else
-      #print "FILE DOES NOT EXISTS"
-        output = File.update(file_name, 'w')
-    end
-      output.puts YAML.dump(records.sort_by{ |id| id.id })
-      output.close
+  if File.file?(file_name)==true
+    # "FILE EXISTS"
+    output = File.new(file_name, 'w')
+  else
+    # print "FILE DOES NOT EXISTS"
+    output = File.update(file_name, 'w')
+  end
+  output.puts YAML.dump(records.sort_by{ |id| id.id })
+  output.close
 end
-
 
 def display_records(arr)
   puts "\n==========================="
   arr.each_with_index do |r, i|
     puts " Record #{i+1} of #{arr.length}"
-      puts "===========================\n"
+    puts "===========================\n"
     puts " ID: #{r.id}\t"
     puts " First Name:\t#{r.first_name} "
     puts " Last Name:\t#{r.last_name}"
     puts " Phone:\t\t#{r.format_phone}"
     puts " Full Name: \t#{r.full_name}"
 
-    if r.addressess.length > 0
-  puts "\n============ ADDRESSES ==========="
-
-          r.addressess.each do |address|
-            puts address.to_s('long')
-
-
+    if r.addressess.length.positive?
+      puts '\n============ ADDRESSES ==========='
+      r.addressess.each do |address|
+        puts address.to_s('long')
       end
-
     end
-    puts "============================"
+    puts '============================'
   end
 end
 
@@ -65,7 +58,7 @@ def get_next_id(arr)
   if arr.length == 0
      id = 1
   else
-    id =  arr.sort_by{ |id| id.id }[-1].id + 1
+    id = arr.sort_by{ |id| id.id }[-1].id + 1
   end
   return id
 end
@@ -81,7 +74,6 @@ def display_record_names(arr)
 end
 
 def display_main_menu
-
   print "\n\n\n===============================\n"
   print "         Main Menu\n"
   print "===============================\n"
@@ -92,14 +84,11 @@ def display_main_menu
   print " 3 - Delete a record\n"
   print " 4 - Display records\n"
   print " 5 - Exit program\n"
-#  print " Testing 9\n"
+  #  print " Testing 9\n"
   print "===============================\n\n\n"
 end
 
-
-
 def display_update_menu
-
   print "\n\n\n===============================\n"
   print " \n How do you wish to edit?\n\n"
   print "===============================\n"
@@ -109,51 +98,50 @@ def display_update_menu
   print " 4 - Edit whole record\n"
   print " 5 - Return to Main Menu\n"
   print "===============================\n"
-
 end
 
 def set_phone_number(person)
-  phoneValid = false
-  while phoneValid == false do
-        print "Enter Phone (10 digit number): "
-        phone = gets.chomp
-        #test to see if the phone number is phoneValid.
-        if phoneValid = (phone.length == 10) && phone.is_i?
-          #person = Person.new(fname, lname, phone, id)
-          person.phone = phone
-        else
-          puts "\nPlease enter a valid phone number. \n"
-        end
+  phone_valid = false
+  while phone_valid == false do
+    print 'Enter Phone (10 digit number): '
+    phone = gets.chomp
+    # test to see if the phone number is phoneValid.
+    if phone_valid = (phone.length == 10) && phone.is_i?
+      # person = Person.new(fname, lname, phone, id)
+      person.phone = phone
+    else
+      puts "\nPlease enter a valid phone number. \n"
+    end
   end
 end
 
 def set_first_name(person)
-    print "Enter First Name:  "
-    person.first_name = gets.chomp
+  print 'Enter First Name:  '
+  person.first_name = gets.chomp
 end
 
 def set_last_name(person)
-  print "Enter Last Name:   "
+  print 'Enter Last Name:   '
   person.last_name = gets.chomp
 end
 
 
 def set_new_address(person)
-  address = Address.new(0,0,0,0,0)
+  address = Address.new(0, 0, 0, 0, 0)
 
-  print "Enter Type of Address: "
+  print 'Enter Type of Address: '
   address.kind = gets.chomp
 
-  print "Enter Street: "
+  print 'Enter Street: '
   address.street = gets.chomp
 
-  print "Enter City: "
+  print 'Enter City: '
   address.city = gets.chomp
 
-  print "Enter Province: "
+  print 'Enter Province: '
   address.province  = gets.chomp
 
-  print "Enter Postal Code: " # do some validation
+  print 'Enter Postal Code: ' # do some validation
   address.postal_code = gets.chomp
 
   person.addressess.push(address)
